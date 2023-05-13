c8ff7a0aa1cae33a01e95d7fd8e63c9e7f8cc78b
wasif_alam <wsfalam84@gmail.com>
2023-05-13 12:04:52 +0530

    completed the task with responsiveness

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const accordionItems = document.querySelectorAll('.accordion-item');
 
+accordionItems.forEach((item) => {
+  const heading = item.querySelector('.accordion-heading');
+  const content = item.querySelector('.accordion-content');
+  const icon = item.querySelector('.accordion-icon');
+
+  heading.addEventListener('click', () => {
+    console.log('click');
+    content.classList.toggle('active');
+    icon.classList.toggle('active');
+  });
+});
