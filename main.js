b2ae31953c18dd0aa26ceba31bc93b0934d805fa
wasif_alam <wsfalam84@gmail.com>
2023-05-13 20:57:08 +0530

    finalized the styles

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Accordion
 const accordionItems = document.querySelectorAll('.accordion-item');
 
 accordionItems.forEach((item) => {
@@ -10,4 +11,13 @@ accordionItems.forEach((item) => {
     content.classList.toggle('active');
     icon.classList.toggle('active');
   });
+});
+
+// Mobiel-Nav
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.mobile-nav');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('active');
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import { defineConfig } from 'vite';
 export default {
   content: ['./*.{html,js}'],
   theme: {
